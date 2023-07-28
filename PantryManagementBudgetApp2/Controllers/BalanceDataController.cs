5d7f5a16c59bda8e68be973d26eb8c767c61f234
jlin1 <jlin1@DESKTOP-J23C7F7>
2023-07-28 10:55:23 -0400

    data controllers created for periods, balances & cashflows

---
+++ PantryManagementBudgetApp2/Controllers/BalanceDataController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PantryManagementBudgetApp2.Models;
+
+namespace PantryManagementBudgetApp2.Controllers
+{
+    public class BalanceDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/BalanceData/ListBalances
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<BalanceDto> ListBalances()
+        {
+            List<Balance> Balances = db.Balances.ToList();
+            List<BalanceDto> BalanceDtos = new List<BalanceDto>();
+
+            Balances.ForEach(bal => BalanceDtos.Add(new BalanceDto()
+            {
+                BalanceId = bal.BalanceId,
+                ChequingAcct = bal.ChequingAcct,
+                SavingsAcct = bal.SavingsAcct,
+                RevolvingCrdt = bal.RevolvingCrdt,
+                InstalmentCrdt = bal.InstalmentCrdt,
+                PeriodId = bal.Period.PeriodId,
+                PeriodYear = bal.Period.PeriodYear,
+                PeriodMonth = bal.Period.PeriodMonth
+            }));
+
+            return BalanceDtos;
+        }
+
+        // GET: api/BalanceData/ListBalancesForPeriod/1
+        [HttpGet]
+        public IEnumerable<BalanceDto> ListBalancesForPeriod(int id)
+        {
+            List<Balance> Balances = db.Balances.Where(b => b.PeriodId == id).ToList();
+            List<BalanceDto> BalanceDtos = new List<BalanceDto>();
+
+            Balances.ForEach(bal => BalanceDtos.Add(new BalanceDto()
+            {
+                BalanceId = bal.BalanceId,
+                ChequingAcct = bal.ChequingAcct,
+                SavingsAcct = bal.SavingsAcct,
+                RevolvingCrdt = bal.RevolvingCrdt,
+                InstalmentCrdt = bal.InstalmentCrdt,
+                PeriodId = bal.Period.PeriodId,
+                PeriodYear = bal.Period.PeriodYear,
+                PeriodMonth = bal.Period.PeriodMonth
+            }));
+
+            return BalanceDtos;
+        }
+
+        // GET: api/BalanceData/FindBalance/5
+        [ResponseType(typeof(Balance))]
+        [HttpGet]
+        [Authorize]
+        public IHttpActionResult FindBalance(int id)
+        {
+            Balance Balance = db.Balances.Find(id);
+            BalanceDto BalanceDto = new BalanceDto()
+            {
+                BalanceId = Balance.BalanceId,
+                ChequingAcct = Balance.ChequingAcct,
+                SavingsAcct = Balance.SavingsAcct,
+                RevolvingCrdt = Balance.RevolvingCrdt,
+                InstalmentCrdt = Balance.InstalmentCrdt,
+                PeriodId = Balance.Period.PeriodId,
+                PeriodYear = Balance.Period.PeriodYear,
+                PeriodMonth = Balance.Period.PeriodMonth
+            };
+            if (Balance == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(BalanceDto);
+        }
+
+        // POST: api/BalanceData/UpdateBalance/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult UpdateBalance(int id, Balance balance)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != balance.BalanceId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(balance).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BalanceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/BalanceData/AddBalance
+        [ResponseType(typeof(Balance))]
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult AddBalance(Balance balance)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Balances.Add(balance);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = balance.BalanceId }, balance);
+        }
+
+        // POST: api/BalanceData/DeleteBalance/5
+        [ResponseType(typeof(Balance))]
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult DeleteBalance(int id)
+        {
+            Balance balance = db.Balances.Find(id);
+            if (balance == null)
+            {
+                return NotFound();
+            }
+
+            db.Balances.Remove(balance);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BalanceExists(int id)
+        {
+            return db.Balances.Count(e => e.BalanceId == id) > 0;
+        }
+    }
+}
---
+++ PantryManagementBudgetApp2/Controllers/PeriodDataController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PantryManagementBudgetApp2.Models;
+
+namespace PantryManagementBudgetApp2.Controllers
+{
+    public class PeriodDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        /// <summary>
+        /// API call that gets all periods from the database periods table
+        /// </summary>
+        /// <returns>
+        /// PeriodId, PeriodYear, PeriodMonth values of all records in database periods table
+        /// </returns>
+        // GET: api/PeriodData/ListPeriods
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<PeriodDto> ListPeriods()
+        {
+            List<Period> Periods = db.Periods.ToList();
+            List<PeriodDto> PeriodDtos = new List<PeriodDto>();
+
+            Periods.ForEach(per => PeriodDtos.Add(new PeriodDto()
+            {
+                PeriodId = per.PeriodId,
+                PeriodYear = per.PeriodYear,
+                PeriodMonth = per.PeriodMonth
+            }));
+
+            return PeriodDtos;
+        }
+
+        /// <summary>
+        /// API call that looks for a specific period in the periods table identified by the id input
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>
+        /// PeriodId, PeriodYear & PeriodMonth of specific record identified by id input
+        /// </returns>
+        // GET: api/PeriodData/FindPeriod/5
+        [ResponseType(typeof(Period))]
+        [HttpGet]
+        [Authorize]
+        public IHttpActionResult FindPeriod(int id)
+        {
+            Period Period = db.Periods.Find(id);
+            PeriodDto PeriodDto = new PeriodDto()
+            {
+                PeriodId = Period.PeriodId,
+                PeriodYear = Period.PeriodYear,
+                PeriodMonth = Period.PeriodMonth
+            };
+            if (Period == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(PeriodDto);
+        }
+
+        // POST: api/PeriodData/UpdatePeriod/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult UpdatePeriod(int id, Period period)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != period.PeriodId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(period).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PeriodExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PeriodData/AddPeriod
+        [ResponseType(typeof(Period))]
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult AddPeriod(Period period)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Periods.Add(period);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = period.PeriodId }, period);
+        }
+
+        // DELETE: api/PeriodData/DeletePeriod/5
+        [ResponseType(typeof(Period))]
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult DeletePeriod(int id)
+        {
+            Period period = db.Periods.Find(id);
+            if (period == null)
+            {
+                return NotFound();
+            }
+
+            db.Periods.Remove(period);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PeriodExists(int id)
+        {
+            return db.Periods.Count(e => e.PeriodId == id) > 0;
+        }
+    }
+}
