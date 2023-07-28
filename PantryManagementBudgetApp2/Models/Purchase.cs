8132a86c57ff1870e6e2b0da420004cd3be90e0f
jlin1 <jlin1@DESKTOP-J23C7F7>
2023-07-28 10:30:00 -0400

    purchase data transfer object created in purchase entity

--- PantryManagementBudgetApp2/Models/Purchase.cs
+++ PantryManagementBudgetApp2/Models/Purchase.cs
@@ -24,4 +24,18 @@ namespace PantryManagementBudgetApp2.Models
         public virtual PantryItem PantryItem { get; set; }
 
     }
+
+    public class PurchaseDto
+    {
+        public int PurchaseID { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Qty { get; set; }
+        public int PeriodId { get; set; }
+        public int PeriodYear { get; set; }
+        public int PeriodMonth { get; set; }
+        public int PantryItemID { get; set; }
+        public string PantryItemName { get; set; }
+        public int PantryItemCurrentQty { get; set; }
+
+    }
 }
