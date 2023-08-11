53ccdcdb1a1f7c9b8c1a1877d1852426f31426d7
yanwing61 <yanwing61@gmail.com>
2023-08-11 10:08:33 -0400

    viewmodels update

--- PantryManagementBudgetApp2/Models/ViewModels/DetailsPeriod.cs
+++ PantryManagementBudgetApp2/Models/ViewModels/DetailsPeriod.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +8,7 @@ namespace PantryManagementBudgetApp2.Models.ViewModels
     public class DetailsPeriod
     {
         public PeriodDto SelectedPeriod { get; set; }
-        public IEnumerable<BalanceDto> RelatedBalances { get; set;}
+        public IEnumerable<BalanceDto> RelatedBalances { get; set; }
         public IEnumerable<CashflowDto> RelatedCashflows { get; set; }
     }
 }
